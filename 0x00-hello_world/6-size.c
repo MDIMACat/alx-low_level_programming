15d920327b26879d8d7fd42d3e0bdd5eda7073f4
MDIMACat <romanmdima@gmail.com>
2023-03-16 07:21:57 -0700

    6-size.c_

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,20 @@
 /**
  * main - Entry point
  *
- * Return: Alway 0 (success);
+ * Return: Always 0 (success)
  */
-
 
 int main(void)
 {
 	int x;
 	float y;
 	char z;
-	printf("size of variable is:%d in bytes", sizeof(x));
-	printf("size of variable is:%f in bytes", sizeof(y));
-	printf("size of variable is:%c in bytes", sizeof(z));
+	long int a;
+	long long int b;
+	printf("Size of int is: %d in bytes\n", sizeof(x));
+	printf("Size of float is: %f in bytes\n", sizeof(y));
+	printf("Size of char is: %c in bytes\n", sizeof(z));
+	printf("Size of long int: %ld in bytes\n", sizeof(a));
+	printf("Size of long long int: %lld in bytes\n", sizeof(b));
 	return (0);
-}
+	}
