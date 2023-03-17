ed12ff7a99e723f25ef0a0a9e6872b4184211cdd
MDIMACat <romanmdima@gmail.com>
2023-03-17 09:48:22 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Description: 'show the Upper case and lower case alphabets'
+ * Return 0 (Success)
+ *
+ */
+int main(void)
+{
+	char i;
+
+	i = 'a'
+		while (i <= 'z')
+		{
+			putchar(i);
+			i++;
+		}
+	i = 'A'
+		while (i <= 'Z')
+		{
+			putchar(i);
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
