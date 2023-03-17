10a9046e4312f209bbfa01583a58be1b2b36ceb2
MDIMACat <romanmdima@gmail.com>
2023-03-17 10:25:12 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	char i;
+
+	i = 'a'
+		while (i <= 'z')
+		{
+			if (c != 'q' && c != 'e')
+			{
+				putchar (i);
+			}
+			c++;
+		}
+	putchar ('\n');
+	return (0);
+}
