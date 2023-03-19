e217d5d837258d1131089c80d8b32f46d6bd5eee
MDIMACat <romanmdima@gmail.com>
2023-03-19 03:37:13 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j = i + 1;
+	int z = i + 2;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (z = i + 2; z <= 9; z++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(z + '0');
+				if (i == 7 && j == 8 && z == 9)
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
