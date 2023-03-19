4ef828fabeb6b5a5c6df5f16c235cdd9a4a503c8
MDIMACat <romanmdima@gmail.com>
2023-03-19 10:02:05 -0700

    101-print_comb4.c__

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int i = 0;
 	int j = i + 1;
-	int z = i + 2;
+	int z = j + 1;
 
 	for (i = 0; i <= 7; i++)
 	{
 		for (j = i + 1; j <= 8; j++)
 		{
-			for (z = i + 2; z <= 9; z++)
+			for (z = j + 1; z <= 9; z++)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
