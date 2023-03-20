adc0a3c65881c57ee6fa099f93c7b69a20c3c25f
MDIMACat <romanmdima@gmail.com>
2023-03-20 15:20:48 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - shows the alphabet characters
+ * @c: is the parameter to be printed
+ *
+ * Return: Always 1 (success) and return 0 if successful
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
