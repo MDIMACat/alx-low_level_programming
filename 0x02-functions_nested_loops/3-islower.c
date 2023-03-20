62843d50ece7f6acff98e4662f5223dfef2352fd
MDIMACat <romanmdima@gmail.com>
2023-03-20 14:22:12 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - shows the lowercase characters
+ * @c: is the parameter to be printed
+ *
+ * Return: Always 1 (success) and return 0 if unsuccessful
+ */
+int _islower(int c)
+{
+	int lower = 'c'
+
+		if (islower(lower) == 0)
+		{
+			_putchar (0 + '0');
+		}
+		else
+		{
+			_putchar(1 + '0');
+		}
+	return (0);
+}
