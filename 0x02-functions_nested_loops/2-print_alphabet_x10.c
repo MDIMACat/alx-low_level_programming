93ba1a43f7b22ffb2ae7d4be2b054ca41f235adc
MDIMACat <romanmdima@gmail.com>
2023-03-20 12:32:38 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 1; j < 11; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	}
+}
