691ccc2ae511f86a4671acb00ac7e8315ab3ee73
MDIMACat <romanmdima@gmail.com>
2023-03-21 04:22:10 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *add - adds two integers
+ *@a: Parameter of function
+ *@b: Parameter of function
+ *Return: Total value of a+b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
