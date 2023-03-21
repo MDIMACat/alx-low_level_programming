aef3798d3f092b4108423b2bc0be9fdb54fa250d
MDIMACat <romanmdima@gmail.com>
2023-03-21 01:33:42 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Shows absolute value
+ * @n: The parameter
+ *
+ * Return: n (absolute value)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		(n = (-1) * n);
+	}
+	else if (n >= 0)
+	{
+		(n == n);
+	}
+	return (n);
+}
