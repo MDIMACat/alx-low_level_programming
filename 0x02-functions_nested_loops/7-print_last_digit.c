2720eb1a68c97b944adfc897eb813736cbfec593
MDIMACat <romanmdima@gmail.com>
2023-03-21 02:24:38 -0700

    7-print_last_digit.c_

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,22 @@
 #include "main.h"
 /**
  * print_last_digit - This prints out the last digits
- * @c: This is the parameter
- *
+ * @i: This is the parameter
  * Return: c%10 the last digit
  */
-int print_last_digit(int c)
+int print_last_digit(int i)
 {
-	return (c % 10);
+	int c;
+
+	c = i % 10;
+	if (c < 0)
+	{
+		_putchar(-c + 48);
+		return (-c);
+	}
+	else
+	{
+		_putchar(c + 48);
+		return (c);
+	}
 }
