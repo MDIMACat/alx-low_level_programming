722856f72364fc02b15dfd72e8bea21332a7cf48
MDIMACat <romanmdima@gmail.com>
2023-03-22 08:30:09 -0700

    9Time

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,27 +6,31 @@
  */
 void times_table(void)
 {
-	int i, j, n;
+	int x;
+	int y;
+	int z;
 
-	for (i = 0; i < 10; i++)
+	for (x = 0; x < 10; x++)
 	{
 		_putchar('0');
-		for (j = 1; j < 10; j++)
 		{
-			n = i * j;
-			_putchar(',');
-			_putchar(' ');
-			if (n <= 9)
+			for (y = 0; y < 10; y++)
 			{
+				z = x * y;
+				_putchar(',');
 				_putchar(' ');
-				_putchar(n + '0');
-			}
-			else
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
+				if (z <= 9)
+				{
+					_putchar(' ');
+					_putchar(z + '0');
+				}
+				else
+				{
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+				}
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar ('\n');
 }
