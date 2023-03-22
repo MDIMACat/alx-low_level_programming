8634e58680d91610eb555b26291b4c60033cd746
MDIMACat <romanmdima@gmail.com>
2023-03-22 00:45:08 -0700

    positive

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,7 +8,7 @@ void positive_or_negative(int i)
         }
         else if (i == 0)
         {
-                printf("%d is zero\n", i);
+                printf("%d is positive\n", i);
         }
         else
         {
