269cb1c9df245bc82dba18a4a7ce53b2b265e9d3
MDIMACat <romanmdima@gmail.com>
2023-03-23 13:39:22 -0700

    buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,27 +4,31 @@
  *
  * Return: void
  */
-int main()
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 15) == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
-		else if ((i % 3) == 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
 		else
 		{
 			printf("%d ", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
 	return (0);
