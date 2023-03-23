987f5b801e95b38671dad923aaa76831fd55f427
MDIMACat <romanmdima@gmail.com>
2023-03-23 05:18:07 -0700

    Print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 /**
  * print_numbers - prints numbers from 0 - 9
  * 
- * Return: Number from 0 to 9
+ * Return: void
  */
 
 void print_numbers(void)
 {
-	int x;
+	char x;
 
-	for (x = 0; x < 10; x++)
+	for (x = '0'; x < '9'; x++)
 	{
-		_putchar(x + '0');
+		_putchar(x);
 	}
 	_putchar('\n');
 
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,7 +6,4 @@
 */
 void more_numbers(void)
 {
-	int i;
-	int j;
 
-	for (i = 0; i < 10; i++)
