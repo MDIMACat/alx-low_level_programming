8382ab5256f6a5a98bc26998fcb720218b7c7dbd
MDIMACat <romanmdima@gmail.com>
2023-03-23 10:06:20 -0700

    6-print

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - printing lines
+ *@n: the Parameter
+ * Return: Always 0 (success)
+ */
+void print_line(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar('_');
+		}
+	}
+}
