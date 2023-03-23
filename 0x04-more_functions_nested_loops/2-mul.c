d496286390874f33fb7f76f3e794654326e6ac2f
MDIMACat <romanmdima@gmail.com>
2023-03-23 01:32:08 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * mul - multiples two integers together
+ * @a: is a parameter for function
+ * @b: is a parameter for function
+ *
+ * Return: Always 0 (success)
+ */
+int mul(int a, int b)
+{
+	int a;
+	int b;
+	int sum;
+
+	sum = a * b;
+	_putchar(sum + '0');
+
+	return (0);
+}
