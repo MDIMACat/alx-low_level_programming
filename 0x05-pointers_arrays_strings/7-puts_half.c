2bf9ad10574c0c2ff4b7f2ce8fb93878321c77d6
MDIMACat <romanmdima@gmail.com>
2023-03-28 02:07:44 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - prints the second half of a given string
+ * @str: Parameter
+ *
+ * Return: Void
+ */
+void puts_half(char *str)
+{
+	char *s = str;
+	int numb = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		s++;
+		numb++;
+	}
+	for (i = 5; i <= numb -1; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
