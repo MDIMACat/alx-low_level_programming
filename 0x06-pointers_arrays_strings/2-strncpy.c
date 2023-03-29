eba1b4b5d7586c4d89544710a77ffd8bde448e9b
MDIMACat <romanmdima@gmail.com>
2023-03-29 10:25:56 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: Parameter destination
+ * @src: Parameter source
+ * @n: Number of characters to be copied
+ *
+ * Return: Destination pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
