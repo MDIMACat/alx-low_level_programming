e6816d0a95ac465033ac5a6d1937ad2ab48ef305
MDIMACat <romanmdima@gmail.com>
2023-03-29 10:43:27 -0700

    3-strcmps.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - comparing 1 string to another string
+ * @s1: Parameter string 1
+ * @s2: Parameter string 2
+ *
+ * Return: 0 if successful
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
