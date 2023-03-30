3af55478b278c910f4713f65a3136d97c43cd0a4
MDIMACat <romanmdima@gmail.com>
2023-03-30 04:15:10 -0700

    6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * cap_string - capitalization of characters
+ * @n: Parameter of string
+ *
+ * Return: n capitalized words
+ */
+char *cap_string(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i])
+	{
+		while (!(n[i] >= 'a' && n[i] <= 'z'))
+		{
+			i++;
+		}
+		if (n[i - 1] == ' ' ||
+				n[i - 1] == '\t' ||
+				n[i - 1] == '\n' ||
+				n[i - 1] == ',' ||
+				n[i - 1] == ';' ||
+				n[i - 1] == '.' ||
+				n[i - 1] == '!' ||
+				n[i - 1] == '?' ||
+				n[i - 1] == '"' ||
+				n[i - 1] == '(' ||
+				n[i - 1] == ')' ||
+				n[i - 1] == '{' ||
+				n[i - 1] == '}' ||
+				i == 0)
+		{
+			n[i] -= 32;
+		}
+	i++;
+	}
+	return (n);
+}
