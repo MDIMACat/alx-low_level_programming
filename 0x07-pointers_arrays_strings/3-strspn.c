a22215e9fcf0fc630643d5ac271e46617a1db521
MDIMACat <romanmdima@gmail.com>
2023-04-03 13:29:30 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: Initia string parameter
+ * @accept: substring parameter
+ *
+ * Return: Always 0 (success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	int b;
+
+	while (*s)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if(*s == accept[b])
+			{
+				i++;
+				break;
+			}
+			else if (accept[b +1] == '\0')
+			{
+				return (i);
+			}
+		}
+		s++;
+	}
+	return (i);
+}
