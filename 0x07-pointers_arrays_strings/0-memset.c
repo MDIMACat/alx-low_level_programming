e96c4236be93e4e7802b6bac6238f0047e61099c
MDIMACat <romanmdima@gmail.com>
2023-04-03 07:30:14 -0700

    0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset -  fills memory with a constant byte.
+ * @s: pointer
+ * @b: parameter
+ * @n: length parameter
+ *
+ * Return: Value of byte
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (n > 0 && n--)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
