f4f19b0c2ddb53f1196061814ffeab8ad71a7045
MDIMACat <romanmdima@gmail.com>
2023-04-05 06:35:43 -0700

    Task_2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prints string length
+ * @s: pointer parameter
+ *
+ * Return: integer string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return ((_strlen_recursion(s) + 1));
+}
