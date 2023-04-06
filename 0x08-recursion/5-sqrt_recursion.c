addd030715f905e11d962c61f882ac7c127dbecb
MDIMACat <romanmdima@gmail.com>
2023-04-06 03:24:14 -0700

    Task_5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,4 +6,36 @@
  *
  * Return: square root or -1 for error
  */
-int help(
+int help(int num, int guess)
+{
+	if ((guess * guess) == num)
+	{
+		return (guess);
+	}
+	else if (guess >= num / 2)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (help(num, guess + 1));
+	}
+}
+#include "main.h"
+/**
+ * _sqrt_recursion - return the square root
+ * @n: number parameter
+ *
+ * Return: Square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (help(n, 1));
+	}
+}
