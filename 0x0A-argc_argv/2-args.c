aff436871c9575f4e2cd931770f351866bbca0ce
MDIMACat <romanmdima@gmail.com>
2023-04-07 05:55:44 -0700

    Task_2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: Argument count parameter
+ * @argv: argument vector parameter
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
