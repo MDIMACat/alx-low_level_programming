6f689798beff680db481e76d3283308fa5d49ff2
MDIMACat <romanmdima@gmail.com>
2023-04-07 04:31:19 -0700

    Task_0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - main function
+ * @argc: argument count parameter
+ * @argv: argument vector parameter
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
