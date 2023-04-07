63ac6f1a88df4f458c338154d39bbb1476ff55eb
MDIMACat <romanmdima@gmail.com>
2023-04-07 04:54:05 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: argument count parameter
+ * @argv: argument vector parameter
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
