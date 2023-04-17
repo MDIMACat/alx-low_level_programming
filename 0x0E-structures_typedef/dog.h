64a9505dfe6cad193e05c225e27f4d3e26562336
MDIMACat <romanmdima@gmail.com>
2023-04-17 06:04:48 -0700

    Task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - this struct prints out dog name, age, owner
+ * @name: name of dog (parameter)
+ * @age: age of dog (parameter)
+ * @owner: the pwner of dog (parameter)
+ */
 
 struct dog
 {
