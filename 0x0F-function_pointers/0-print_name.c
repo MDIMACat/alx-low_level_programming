1ee7d904ae743456a4d612d511481de0c3b960d9
MDIMACat <romanmdima@gmail.com>
2023-04-19 07:25:38 -0700

    Updated task 0, using NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - function to print out names
  * @name: is a char name parameter
@@ -7,7 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
