28d2cd272e0369d6845846095da63179b16691be
MDIMACat <romanmdima@gmail.com>
2023-04-19 08:00:00 -0700

    Updated Task 0, added spacing

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,15 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+
 /**
  * print_name - function to print out names
  * @name: is a char name parameter
  * @f: function pointer parameter
  * Return: Void
- * 
+ *
  */
+
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
