fb72b8082a66a59864ac6e52b81ae378d8dd7650
MDIMACat <romanmdima@gmail.com>
2023-04-28 08:58:45 -0700

    Task 4 free list_t

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees list_t
+ * @head: first node pointer
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
