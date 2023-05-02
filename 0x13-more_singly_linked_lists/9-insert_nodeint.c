4db23f5a10463d725063b0e2c266c89a4debc3a8
MDIMACat <romanmdima@gmail.com>
2023-05-02 13:42:51 -0700

    Updated task 9 - insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,6 +38,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		temp = temp->next;
 	}
+	if (temp == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
 	newNode->next = temp->next;
 	temp->next = newNode;
 	return (newNode);
