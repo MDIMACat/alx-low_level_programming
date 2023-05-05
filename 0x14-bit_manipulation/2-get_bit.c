062fd113054a617ec50996e442b483232b70e01a
MDIMACat <romanmdima@gmail.com>
2023-05-05 08:06:14 -0700

    Task 2 for project 0x14 Bit Manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - gets value of a bit at a given index.
+ * @n: value parameter
+ * @index: index parameter
+ * Return: 1 for on and 0 for off
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int)*8)
+	{
+		return (-1);
+	}
+	return (n >> index) & 1;
+}
