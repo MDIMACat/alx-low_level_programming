9b0aa2d7f354f91231598573c6de49b5388165ef
MDIMACat <romanmdima@gmail.com>
2023-05-08 04:03:15 -0700

    Updated task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,24 +20,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
 	bytes_read = read(fd, buf, letters);
 	if (bytes_read == -1)
 	{
-		free(buffer);
+		free(buf);
 		close(fd);
 		return (0);
 	}
-	buffer[bytes_read] = '\0';
-	if (write(STDOUT_FILENO, buf, byte_read) != bytes_read)
+	buf[bytes_read] = '\0';
+	if (write(STDOUT_FILENO, buf, bytes_read) != bytes_read)
 	{
-		fress(buffer);
+		free(buf);
 		close(fd);
 		return (0);
 	}
-	free(buffer);
+	free(buf);
 	close(fd);
 	return (bytes_read);
 }
