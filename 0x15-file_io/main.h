98ff1dff5f6120498528679c67a23db460432026
MDIMACat <romanmdima@gmail.com>
2023-05-09 02:11:36 -0700

    updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,7 @@
 #include <string.h>
 #include <errno.h>
 
-int main (int argc , char *argv[]);
+int main(int argc , char *argv[]);
 void print_err(char *msg, char *filename, int exit_code);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
