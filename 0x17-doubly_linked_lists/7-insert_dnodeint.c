1a7fe19fd02d57f4251c2ca1f9536cc4c2433087
MDIMACat <romanmdima@gmail.com>
2023-06-15 14:34:54 +0200

    Task 7- inserting node at a given point

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert a new node at index
+ * @h: pointer parameter
+ * @idx: index pointer parameter
+ * @n: data pointer parameter
+ * Return: list of nodes
+*/
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int count = 0;
+	dlistint_t *newP = NULL;
+	dlistint_t *temp;
+
+	if (*h == NULL)
+	{
+		return (NULL);
+	}
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	temp = *h;
+	while (temp != NULL && count < idx - 1)
+	{
+		temp = temp->next;
+		count++;
+	}
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	if (temp->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	newP = malloc(sizeof(dlistint_t));
+	if (newP == NULL)
+	{
+		return (NULL);
+	}
+	newP->n = n;
+	newP->prev = temp;
+	newP->next = temp->next;
+	temp->next->prev = newP;
+	temp->next = newP;
+	return (newP);
+}
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -16,3 +16,5 @@
         Write a function that returns the nth node of a dlistint_t linked list.
     6. Sum list 
         Write a function that returns the sum of all the data (n) of a dlistint_t linked list.
+    7. Insert at index 
+        Write a function that inserts a new node at a given position.
