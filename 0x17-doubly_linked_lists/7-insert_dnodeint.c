9a22d5a514e42a8f61cb2d1aba43dbedd9c8e22a
MDIMACat <romanmdima@gmail.com>
2023-06-15 14:40:44 +0200

    Updated code for task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		count++;
 	}
-	if (temp == NULL)
+	if (temp == NULL || count != idx - 1)
 	{
 		return (NULL);
 	}
