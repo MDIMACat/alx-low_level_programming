a597552e7e7fdfab7243eea0c5bfd1d6279e0166
MDIMACat <romanmdima@gmail.com>
2023-06-19 14:30:26 +0200

    Task 4

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo -e '#include <stdio.h>\nunsigned int rand(void) { return 42; }\n' > fake_rand.c
+gcc -shared -o fake_rand.so -fPIC fake_rand.c
---
+++ 0x18-dynamic_libraries/fake_rand.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+unsigned int rand(void) { return 42; }
+
