f7e7e0fbbb711fc65e36cfc7e1a85c2b7f363abb
MDIMACat <romanmdima@gmail.com>
2023-06-19 14:22:45 +0200

    Task 2 of dynamic libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ * add - adds to integers
+ * a: integer parameter
+ * b: integer parameter
+ * Return: sum of two numbers
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtracts two integers
+ * a: int parameter
+ * b: int parameter
+ * Return: Subtracted value of a and b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two values
+ * a: int parameter
+ * b: int parameter
+ * Return: multiplied value
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two values
+ * a: int parameter
+ * b: int parameter
+ * Return: Divided value
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - gets remainder of two values
+ * a: int parameter
+ * b: int parameter
+ * Return: remainder
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
