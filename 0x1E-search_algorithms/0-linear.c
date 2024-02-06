5da02725f27d8e9852d974623b927562784b8a34
MDIMACat <romanmdima@gmail.com>
2024-02-06 08:22:31 +0000

    Changed placeholder from %d to %u

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", array[i], value);
+		printf("Value checked array[%u] = [%d]\n", array[i], value);
 		if (value == array[i])
 		{
 			return (i);
