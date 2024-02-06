ac736b2c5c1e1338ea5dee042b43d462e5ecabe5
MDIMACat <romanmdima@gmail.com>
2024-02-06 08:08:40 +0000

    Jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - Jump search alogrithm function
+ * @array: The sorted array to be searched
+ * @size: The size of the array
+ * @value: The value to be found in the array
+ * Return: -1 on failure and 0 on success
+*/
+
+int jump_search(int *array, size_t size, int value)
+{
+	int x = 0, i = 0, m =0;
+
+	if (array == NULL)
+		return (-1);
+	m = (int)sqrt(size);
+	while (array[m] <= value && m < size)
+	{
+		i = m;
+		m += sqrt(size);
+		if (m > size - 1)
+			return (-1);
+	}
+	for (x = i; x < m ;x++)
+	{
+		printf("Value checked array[%d] = [%d]", x, array[x]);
+		if (value == array[x])
+		{
+			printf("Value found between indexes [%d] and [%d]", i, x);
+			return (x);
+		}
+	}
+	return (-1);
+}
